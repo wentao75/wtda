1e7bde5c406465fd3e5338401448199b2f94f0c7
温涛 <wentao@WTHRMacmini.local>
2020-05-23 16:05:04 +0800

    添加cli-ux库，支持交互确认

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,12 +1,15 @@
 const { Command } = require("@oclif/command");
 const { clearAllData } = require("@wt/lib-wtda");
+const { cli } = require("cli-ux");
 
 class ClearCommand extends Command {
     async run() {
-        this.log("开始清理数据...");
         try {
-            await clearAllData();
-            this.log("数据清理完毕！");
+            if (await cli.confirm("确定要删除全部数据吗？( y/n )")) {
+                this.log("开始清理数据...");
+                await clearAllData();
+                this.log("数据清理完毕！");
+            }
         } catch (error) {
             this.log("清理数据过程中发生错误:", error);
         }
